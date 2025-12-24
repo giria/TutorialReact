4fc50fcd4ecb4884efec277b54a703293cbac133
Joan Barrull Ribalta <giria@users.noreply.github.com>
2025-12-24 15:40:59 +0100

    form submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react"
 
 function App() {
-  const [text, setText] = useState("")
+  const [name, setName] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`Hello ${name}`)
+  }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
-
-      <p>You typed: {text}</p>
-    </div>
+      <button type="submit">Submit</button>
+    </form>
   )
 }
 
