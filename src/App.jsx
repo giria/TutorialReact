374c923f6f55ddec124ba2bf726107255578eb55
Joan Barrull Ribalta <giria@users.noreply.github.com>
2025-12-24 18:13:30 +0100

    async/await version (preferered)

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import { useState, useEffect } from "react"
 function App() {
   const [users, setUsers] = useState([])
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(data => setUsers(data))
-  }, [])
+ useEffect(() => {
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users")
+      const data = await res.json()
+      setUsers(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  fetchUsers()
+}, [])
 
   return (
     <ul>
