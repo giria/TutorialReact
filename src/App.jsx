3a52b6869e503d36bdb4ab466ac1f08b8236f73e
Joan Barrull Ribalta <giria@users.noreply.github.com>
2025-12-24 15:36:19 +0100

    Inputs and forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <p>You typed: {text}</p>
     </div>
   )
 }
