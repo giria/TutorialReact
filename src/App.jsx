5831b63f6a11813f39bc247f19b4a7a855e32927
Joan Barrull Ribalta <giria@users.noreply.github.com>
2025-12-24 15:58:07 +0100

    add value

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react"
 
 function App() {
-  const [todos, setTodos] = useState(["Learn React", "Practice"])
+  const [todos, setTodos] = useState(["Apple", "Orange"])
 
   return (
+    <>
+    <div>
     <ul>
       {todos.map((todo, index) => (
         <li key={index}>{todo}</li>
       ))}
     </ul>
+    </div>
+    <div>
+    <button onClick={() => setTodos([...todos, "Banana"])}>Add Banana</button>
+    </div>
+    </>
+
   )
 }
-
 export default App
